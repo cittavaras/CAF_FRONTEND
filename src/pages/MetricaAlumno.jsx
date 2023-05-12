f04704a4013eff817645225e9590504669cfe410
diego alejandro pena gonzalez <dapg.036@outlook.com>
2023-05-12 18:00:58 -0400

    traermetricasporfecha

--- src/pages/MetricaAlumno.jsx
+++ src/pages/MetricaAlumno.jsx
@@ -19,6 +19,8 @@ import {
 
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet"></link>
 
+
+
 const Metrica = () => {
   const [metricas, setMetricas] = useState([]);
   useEffect(() => {
@@ -40,24 +42,26 @@ const Metrica = () => {
     setMetricas(metricaAlumno);
   }
 
-  const datas = [
-    { name: 'Byron', edad: 24, altura: 1.80 },
-    { name: 'Javier', edad: 23, altura: 1.70 },
-    { name: 'Cristian', edad: 22, altura: 1.60 },
-    { name: 'Jorge', edad: 21, altura: 1.50 },
-    { name: 'Javiera', edad: 20, altura: 1.40 },
-  ]
   const dataGrafico = [
     { name: 'Edad', uv: metricas?.edad ?? 0, pv: 2400, amt: 2400, },
     { name: 'Altura', uv: metricas?.altura ?? 0, pv: 1398, amt: 2210, },
     { name: 'Peso corporal', uv: metricas?.peso ?? 0, pv: 9800, amt: 2290, },
     { name: 'Porcentaje de grasa corporal', uv: metricas?.porcentajeGrasaCorporal ?? 0, pv: 3908, amt: 2000, },
     { name: 'Porcentaje de músculo', uv: metricas?.porcentajeGrasaMuscular ?? 0, pv: 4800, amt: 2181, },
-
     { name: 'Índice de masa corporal (IMC)', uv: metricas?.imc ?? 0, pv: 3800, amt: 2500, },
     { name: 'Grasa visceral', uv: metricas?.grasaVisceral ?? 0, pv: 4300, amt: 2100, },
   ];
+/*
+quiero hacer un boton que cambia los valores de la tabla de metricas al alumno, ordenando por fecha
+*/
 
+  const getMetricasByFecha = async (fecha) => {
+    const { rut } = JSON.parse(sessionStorage.getItem('alumno_sesion'));
+    const res = await axios.post(baseURL + '/metricas/alumno', { rut });
+    const metrics = res.json();
+    const metricasByFecha = metrics.filter(metrica => metrica.fecha === fecha);
+    setMetricas(metricasByFecha);
+  };
 
   // Datos de ejemplo
   const data = React.useMemo(
@@ -79,18 +83,29 @@ const Metrica = () => {
     [metricas]
   );
 
-
   const columns = React.useMemo(
     () => [{ Header: 'Métricas de seguimiento del alumno', accessor: 'metrica' },
-    { Header: 'Valor', accessor: 'valor' }],
-    []
+    { 
+      Header: 'Valor',
+      accessor: 'valor', 
+    },
+    {
+      header: 'ver metricas', accessor: 'fecha',
+      Cell: ({ row }) => (
+        <button onClick={() => handleVerMetricas(row.original.fecha)}>
+          Ver
+        </button>
+        ),
+      },
+    ],
+    [],
   );
 
   const tableInstance = useTable({
     columns,
     data
   });
-
+  // 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
 
 
@@ -111,7 +126,7 @@ const Metrica = () => {
     }
   
     const filteredData = data.filter(item => item.name === selectedMetric);
-  
+    
     return (
       <div>
         <h2>{selectedMetric}</h2>
