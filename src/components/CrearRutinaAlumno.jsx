959d0fc5fc50ea2ceb65b355c728dd957d332fe7
victsilva <vict.silva@duocuc.cl>
2023-07-07 21:24:57 -0400

    Se agregó eel componente de crear rutina del alumno

---
+++ src/components/CrearRutinaAlumno.jsx
@@ -0,0 +1,426 @@
+import React, { useState, useEffect } from 'react';
+import baseURL from '../helpers/rutaBase';
+import axios from 'axios';
+import {
+    Button, Typography, Grid,
+    Select, IconButton, MenuItem,
+    Autocomplete, InputLabel, TableContainer,
+    Table, TableHead, TableRow,
+    TableCell, TableBody, TextField, FormControl
+} from "@mui/material";
+import { Link } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+import { Container, Box, bgcolor, height } from '@mui/system';
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import { Modal } from '@mui/material';
+import useAuth from '../auth/useAuth';
+import moment from 'moment';
+import PlusOneIcon from '@mui/icons-material/PlusOne';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const CrearRutinaAlumno = () =>{
+    const [nomRutina, setNomRutina] = useState("");
+    const fechaActual = moment().format('DD-MM-YYYY');
+    const [alumnoSeleccionado, setAlumnoSeleccionado] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
+    const { alumno } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getAlumnos();
+      })
+
+      useEffect(() => {
+        if (alumnoSeleccionado) {
+            // Aquí puedes hacer las validaciones y asignaciones necesarias
+            getRutinas();
+            console.log('Alumno seleccionado', alumnoSeleccionado);
+        }
+    }, [alumnoSeleccionado]);
+    
+      // Función para obtener la lista de alumnos
+      const getAlumnos = async () => {
+        try {
+          const res = await axios.get(baseURL + '/alumnos');
+          const alumnos = res.data.alumnos.filter(alumno => alumno.tipoUsuario === 'Alumno' && alumno.active === true);
+          return alumnos
+        } catch (error) {
+          //console.log(error);
+        }
+      }
+
+      const getRutinas = async () => {
+
+        const res = await axios.get(baseURL + '/rutinas/alumno', { params: { rut: alumnoSeleccionado.rut } });
+        //  console.log(rut);
+        //   console.log(res);
+        //  // console.log("Todas las metricas",res.data)
+        const rutinasAlumno = res.data;
+        console.log("rutinasAlumno", rutinasAlumno)
+        setRutinas(rutinasAlumno);
+        /*
+        if (rutinasAlumno.length > 0) {
+            const rutinaDefault = rutinasAlumno[0];
+            setCardioInicial(rutinaDefault.cardioInicial);
+            setCardioFinal(rutinaDefault.cardioFinal);
+            setCalentamiento(rutinaDefault.calentamiento);
+            setVueltaALaCalma(rutinaDefault.vueltaALaCalma);
+            setNomRutina(rutinaDefault.nombre);
+            setSelectedExercises(generarEjercicios(rutinaDefault.ejercicios))
+        }
+        */
+
+    }
+
+
+      const [selectedRutina, setSelectedRutina] = useState([]);
+      const [rutinas, setRutinas] = useState([]);
+      const [cardioInicial, setCardioInicial] = useState("");
+      const [cardioFinal, setCardioFinal] = useState("");
+      const [calentamiento, setCalentamiento] = useState("");
+      const [vueltaALaCalma, setVueltaALaCalma] = useState("");
+      const [selectedExercises, setSelectedExercises] = useState([]);
+      const [selectedRepeticion, setSelectedRepeticion] = useState([]);
+      const [selectedSerie, setSelectedSerie] = useState([]);
+      const [selectedKg, setSelectedKg] = useState([]);
+      const [selectedDescanso, setSelectedDescanso] = useState([]);
+
+      const ejercicios = [
+        { id: 1, nombre: "Flexiones de brazo apoyo" },
+        { id: 2, nombre: "Rodillas" },
+        { id: 3, nombre: "Elevaciones de hombro" },
+        { id: 4, nombre: "Frontales con disco" },
+        { id: 5, nombre: "Dorsalera" },
+        { id: 6, nombre: "Remo polea" },
+        { id: 7, nombre: "Pall off press" },
+        { id: 8, nombre: "Prensa 45°" },
+        { id: 9, nombre: "Extensión de rodilla" },
+        { id: 10, nombre: "Máquina Cuadríceps" },
+        { id: 11, nombre: "Puente isquiotibiales" },
+        { id: 12, nombre: "Press banca" },
+        { id: 13, nombre: "Press de hombros" },
+        { id: 14, nombre: "Mancuernas" },
+        { id: 15, nombre: "Dorsalera polea" },
+        { id: 16, nombre: "Remo mancuernas" },
+        { id: 17, nombre: "Estocadas" },
+        { id: 18, nombre: "Peso muerto Rumano" },
+        { id: 19, nombre: "Búlgaras" },
+        { id: 20, nombre: "Hip Thrust" },
+        { id: 21, nombre: "Elevaciones de talones" },
+        { id: 22, nombre: "Smith" },
+        { id: 23, nombre: "Crunch Abdominal" },
+        { id: 24, nombre: "Puente isquiotibiales" }
+    ];
+
+    const handleCardioInicialChange = (event) => {
+        setCardioInicial(event.target.value);
+    };
+    const handleCardioFinalChange = (event) => {
+        setCardioFinal(event.target.value);
+    };
+    const handleCalentamientoChange = (event) => {
+        setCalentamiento(event.target.value);
+    };
+    const handleVueltaALaCalmaChange = (event) => {
+        setVueltaALaCalma(event.target.value);
+    };
+
+
+    const handleExerciseChange = (index, value) => {
+        const updatedExercises = [...selectedExercises];
+        updatedExercises[index] = value;
+        setSelectedExercises(updatedExercises);
+    };
+    const handleRepeticionChange = (index, value) => {
+        const updatedRepeticion = [...selectedRepeticion];
+        updatedRepeticion[index] = value;
+        setSelectedRepeticion(updatedRepeticion);
+    };
+    const handleSerieChange = (index, value) => {
+        const updatedSerie = [...selectedSerie];
+        updatedSerie[index] = value;
+        setSelectedSerie(updatedSerie);
+    };
+    const handleKgChange = (index, value) => {
+        const updatedKg = [...selectedKg];
+        updatedKg[index] = value;
+        setSelectedKg(updatedKg);
+    };
+    const handleDescansoChange = (index, value) => {
+        const updatedDescanso = [...selectedDescanso];
+        updatedDescanso[index] = value;
+        setSelectedDescanso(updatedDescanso);
+    };
+
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+      };
+    
+    
+    const registrarRutina = async (e) => {
+        e.preventDefault();
+        console.log("alumno", alumno);
+
+        try {
+            const res = await axios.post(baseURL + '/rutinas/alumno/', {
+                nombre: nomRutina,
+                instructorId: "",
+                alumnoId: alumno.rut,
+                cardioInicial: cardioInicial,
+                cardioFinal: cardioFinal,
+                calentamiento: calentamiento,
+                vueltaALaCalma: vueltaALaCalma,
+                ejercicios: getEjercicios(),
+            });
+
+            // Mostrar el alert
+            alert('Rutina registrada');
+
+            // Redirigir al usuario a la página anterior
+            navigate(-1);
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+
+
+
+
+    const getEjercicios = () => {
+        const ejerciciosFormateados = selectedRutina.map((ejercicio) => {
+            return {
+                nombre: selectedExercises[ejercicio],
+                repeticiones: selectedRepeticion[ejercicio],
+                series: selectedSerie[ejercicio],
+                kg: selectedKg[ejercicio],
+                descanso: selectedDescanso[ejercicio],
+            }
+        }
+        )
+        return ejerciciosFormateados;
+    }
+    const generarEjercicios = (ejercicios = []) => {
+        setSelectedExercises(ejercicios.map(ejercicio => ejercicio.nombre));
+        setSelectedRepeticion(ejercicios.map(ejercicio => ejercicio.repeticiones));
+        setSelectedSerie(ejercicios.map(ejercicio => ejercicio.series));
+        setSelectedKg(ejercicios.map(ejercicio => ejercicio.kg));
+        setSelectedDescanso(ejercicios.map(ejercicio => ejercicio.descanso));
+        setSelectedRutina(ejercicios.map((ejercicio, index) => index));
+    }
+
+  
+
+    
+    return (
+        
+        
+        <>
+          <div style={{ backgroundColor: "white"}} >
+            <div className=' container py-5  text-center'>
+              <h1>{nomRutina}</h1>
+              <Grid item xs={12} sm={6}>
+                  <div>
+                    <Typography variant="subtitle1" component="h3" sx={{ fontSize: '20px' }}>
+                      Nombre: {alumno?.nombre ?? 'Sin informacion'}
+                    </Typography>
+
+                    <Typography variant="subtitle1" component="h3" sx={{ fontSize: '20px' }}>
+                      Fecha Rutina: {fechaActual}
+                    </Typography>
+                  </div>
+                </Grid>
+              
+              <Grid item xs={12} sm={6}>
+                  <div className="form-group" style={{ marginTop: '20px' }}>
+                  <FormControl style={{ width: '50%' }}>
+                        <InputLabel>Seleccione una rutina</InputLabel>
+                        <Select
+                          name="rutina"
+                          value={nomRutina}
+                          onChange={(e) => setNomRutina(e.target.value)}
+                          
+                        >
+                          <MenuItem value="Rutina 1">Rutina 1</MenuItem>
+                          <MenuItem value="Rutina 2">Rutina 2</MenuItem>
+                          <MenuItem value="Rutina 3">Rutina 3</MenuItem>
+                        </Select>
+                      </FormControl>
+                  </div>
+                </Grid>
+                <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Typography variant="subtitle1" component="h3" sx={{ fontSize: '20px' }}>
+                    Cardio Inicial
+                  </Typography>
+                  <TextField
+                    id="outlined-multiline-static"
+                    label="ej: 10 minutos"
+                    value={cardioInicial}
+                    type='number'
+                    onChange={handleCardioInicialChange}
+                    style={{ width: '50%' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Typography variant="subtitle1" component="h3" sx={{ fontSize: '20px' }}>
+                    Cardio Final
+                  </Typography>
+                  <TextField
+                    id="outlined-multiline-static"
+                    label="ej: 10 minutos"
+                    value={cardioFinal}
+                    type='number'
+                    onChange={handleCardioFinalChange}
+                    style={{ width: '50%' }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} style={{margin: "auto"}}>
+                <Typography variant="subtitle1" component="h3" sx={{ fontSize: '20px' }}>
+                    Calentamiento
+                  </Typography>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Descripcion del calentamiento"
+                    multiline
+                    rows={3}
+                    value={calentamiento}
+                    onChange={handleCalentamientoChange}
+                    style={{ width: '100%' }}
+                  />
+                  </Grid>
+                <Grid item xs={12} sm={6} style={{margin: "auto"}}>
+                <Typography variant="subtitle1" component="h3" sx={{ fontSize: '20px' }}>
+                    Vuelta a la calma
+                  </Typography>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Descripcion de la vuelta a la calma"
+                    multiline
+                    rows={3}
+                    value={vueltaALaCalma}
+                    onChange={handleVueltaALaCalmaChange}
+                    style={{ width: '100%' }}
+                  />
+                  </Grid>
+              </Grid>
+
+             
+              
+              <TableContainer style={{}}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Ejercicios</TableCell>
+                      <TableCell>Repeticiones</TableCell>
+                      <TableCell>Kg</TableCell>
+                      <TableCell>Series</TableCell>
+                      <TableCell>Descanso</TableCell>
+                      <TableCell>Acciones</TableCell>
+                    </TableRow>
+                  </TableHead>
+
+                  <TableBody>
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="success" startIcon={<PlusOneIcon />} onClick={() => setSelectedRutina([...selectedRutina, selectedRutina.length])}>Agregar Ejercicio</Button>
+                      </Grid>
+                      {selectedRutina.map((ejercicio) => (
+                        <TableRow key={ejercicio}>
+                          <TableCell>
+                            <FormControl style={{ width: '100%', maxWidth: '200px' }}>
+                              <InputLabel>Seleccione Ejercicio</InputLabel>
+                              <Select
+                                name="rutina"
+                                type="text"
+                                value={selectedExercises[ejercicio]}
+                                onChange={(e) => handleExerciseChange(ejercicio, e.target.value)}
+                                style={{ width: '100%' }}
+                              >
+                                {ejercicios.map((ejercicio) => (
+                                  <MenuItem key={ejercicio.id} value={ejercicio.nombre}>
+                                    {ejercicio.nombre}
+                                  </MenuItem>
+                                ))}
+                              </Select>
+                            </FormControl>
+                          </TableCell>
+                          <TableCell>
+                            <TextField
+                              type="int"
+                              label="repeticiones"
+                              variant="outlined"
+                              fullWidth
+                              placeholder="Ejemplo: 20"
+                              value={selectedRepeticion[ejercicio]}
+                              onChange={(e) => handleRepeticionChange(ejercicio, e.target.value)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <TextField
+                              type="float"
+                              label="Kg"
+                              variant="outlined"
+                              fullWidth
+                              placeholder="Ejemplo: 20"
+                              value={selectedKg[ejercicio]}
+                              onChange={(e) => handleKgChange(ejercicio, e.target.value)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <TextField
+                              type="int"
+                              label="series"
+                              variant="outlined"
+                              fullWidth
+                              placeholder="Ejemplo: 20"
+                              value={selectedSerie[ejercicio]}
+                              onChange={(e) => handleSerieChange(ejercicio, e.target.value)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <TextField
+                              type="float"
+                              label="Descanso"
+                              variant="outlined"
+                              fullWidth
+                              placeholder="Ejemplo: 20"
+                              value={selectedDescanso[ejercicio]}
+                              onChange={(e) => handleDescansoChange(ejercicio, e.target.value)}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <IconButton aria-label="delete" size="small" color="error" onClick={() => setSelectedRutina(selectedRutina.filter((e) => e !== ejercicio))}>
+                              <DeleteIcon />
+                            </IconButton>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                
+                </Table>
+              </TableContainer>
+              
+              <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+                  <Button autoFocus color="success" variant="contained" onClick={registrarRutina} >
+                    Agregar 
+                  </Button>
+                </div>
+                <br />
+                
+                
+                
+            </div>                           
+          </div>
+    
+        </>
+        
+
+   
+    )
+}
+
+export default CrearRutinaAlumno;
